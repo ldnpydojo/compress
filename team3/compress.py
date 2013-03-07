b95966a46ea2e9dccd473c40ab802b86774dec54
markeganfuller <mark.egan-fuller@mvfglobal.com>
2013-03-07 20:51:38 +0000

    Cut down to high freq words only.

--- team3/compress.py
+++ team3/compress.py
@@ -9,7 +9,12 @@ frequencies = collections.defaultdict(int)
 for word in words:
     frequencies[word] += 1
 
+high_frequencies = {}
 
-sorted_frequencies = sorted(frequencies, key=frequencies.get, reverse=True)
+for word in frequencies:
+    if frequencies[word] > 5:
+        high_frequencies[word] = frequencies[word]
+
+sorted_frequencies = sorted(high_frequencies, key=high_frequencies.get, reverse=True)
 
 print sorted_frequencies
