a1e4336d68dfabd4c5d761d17e025d814344bc0b
TomV <tom@viner.tv>
2013-03-10 00:18:54 +0000

    tidy and construct better regex

--- team1/compressor.py
+++ team1/compressor.py
@@ -1,23 +1,27 @@
 import re
+import sys
 from collections import Counter
-import pickle
+import cPickle
 
 def encode(words):
     counted_words = Counter(words)
-    #scores = {word: len(word) * counted_words[word] for word in set(words)}
-
     ixs_to_words = dict(enumerate(k for (k,v) in counted_words.most_common()))
     words_to_ixs = {v: k for k, v in ixs_to_words.items()}
 
     cipher = []
     for word in words:
         if counted_words[word] == 1:
-            print word
             cipher.append(word)
+            del ixs_to_words[words_to_ixs[word]]
         else:
             cipher.append(words_to_ixs[word])
-    print 'dictionary:', str(ixs_to_words)[:100], '...', str(ixs_to_words)[-100:]
-    print 'ciphertext', str(cipher)[:100]
+
+    print 'Input: %s words, %s chars' % (len(words), len(''.join(words)))
+    print 'dictionary (%s items): %s\t...%s' % (
+        len(ixs_to_words),
+        str(ixs_to_words)[:200], str(ixs_to_words)[-100:]
+    )
+    print 'ciphertext (%s items): %s' % (len(cipher), str(cipher)[:100])
     return ixs_to_words, cipher
 
 def decode(ixs_to_words, cipher):
@@ -29,22 +33,23 @@ def decode(ixs_to_words, cipher):
             decoded.append(k)
     return decoded
 
-def encode_file(path1, path2):
+def encode_file(path1, path2,
+        word_re=r"(?:\w+(?:[':-]\w+)? ?)|(?:\r|\n)+|. +|."):
     #words = open(path1).read().split()
-    words = re.findall(r"[\w']+|[.,!?;]", open(path1).read())
+    words = re.findall(word_re, open(path1).read())
     encoded = encode(words)
     with open(path2, 'w') as f:
-        pickle.dump(encoded, f, pickle.HIGHEST_PROTOCOL)
+        cPickle.dump(encoded, f, cPickle.HIGHEST_PROTOCOL)
 
 def decode_file(path1, path2):
     with open(path1) as f:
-        ixs_to_words, cipher = pickle.load(f)
+        ixs_to_words, cipher = cPickle.load(f)
     words = decode(ixs_to_words, cipher)
     with open(path2, 'w') as f:
-        f.write(' '.join(words))
+        f.write(''.join(words))
+
 
 if __name__ == '__main__':
-    import sys
     if sys.argv[1] == 'encode':
         encode_file(sys.argv[2], sys.argv[3])
     else:
